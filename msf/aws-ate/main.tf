7205a8fcf526656aec77b7904bc55c5f976f8ab6
HinhNguyen <hinhnguyen00@gmail.com>
2019-03-19 17:43:52 +0700

    create VPC

--- msf/aws-ate/main.tf
+++ msf/aws-ate/main.tf
@@ -1,6 +1,26 @@
-resource "aws_vpc" "prd-ate" {
-  cidr_block = "100.1.0.0/16"
+# Create VPC
+resource "aws_vpc" "ate-vpc" {
+  cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "prd-ate"
+    Name = "${var.environment}-${var.project}"
+  }
+}
+
+# Create subnet
+resource "aws_subnet" "ate-public-1a" {
+  vpc_id = "${aws_vpc.ate-vpc.id}"
+  availability_zone = "us-east-1a"
+  cidr_block = "10.0.1.0/24"
+  tags = {
+    Name = "${var.environment}-${var.project}-public-1a"
+  }
+}
+
+resource "aws_subnet" "ate-private-1a" {
+  vpc_id = "${aws_vpc.ate-vpc.id}"
+  availability_zone = "us-east-1a"
+  cidr_block = "10.0.101.0/24"
+  tags = {
+    Name = "${var.environment}-${var.project}-private-1a"
   }
 }
---
+++ msf/aws-ate/var.tf
@@ -0,0 +1,8 @@
+variable "environment" {
+  description = "Define environment e.g prd, stg, dev, etc"
+}
+
+variable "project" {
+  default = "ate"
+  description = "Define defaut value for project"
+}
